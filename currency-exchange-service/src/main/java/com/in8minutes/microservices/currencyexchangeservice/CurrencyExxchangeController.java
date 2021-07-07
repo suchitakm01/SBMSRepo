0a6d035c729c045fd0ff74ab0b1439a6bd4e712c
suchitakm01 <54839354+suchitakm01@users.noreply.github.com>
2021-07-07 14:32:00 +0530

    Dynamic port in response using Environment

--- currency-exchange-service/src/main/java/com/in8minutes/microservices/currencyexchangeservice/CurrencyExxchangeController.java
+++ currency-exchange-service/src/main/java/com/in8minutes/microservices/currencyexchangeservice/CurrencyExxchangeController.java
@@ -2,6 +2,8 @@ package com.in8minutes.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExxchangeController {
 	
+	@Autowired
+	private Environment env;
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExxchange getCurrencyExchange(
 			@PathVariable String from,
 			@PathVariable String to) {
-		return new CurrencyExxchange(1000l, from, to, BigDecimal.valueOf(75));
+		CurrencyExxchange currencyExxchange = new CurrencyExxchange(1000l, from, to, BigDecimal.valueOf(75));
+		currencyExxchange.setEnvironment(env.getProperty("local.server.port"));
+		return currencyExxchange;
 	}
 }
